1f8f4111558d08705b191d0bd3a80b98752ba576
mperaic <mperaic@gmail.com>
2018-07-05 23:04:09 +0200

    migrate users reset password

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_07_02_154418) do
-=======
 ActiveRecord::Schema.define(version: 2018_07_05_181330) do
 
   create_table "microposts", force: :cascade do |t|
@@ -24,7 +21,6 @@ ActiveRecord::Schema.define(version: 2018_07_05_181330) do
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
->>>>>>> user-microposts
 
   create_table "users", force: :cascade do |t|
     t.string "name"
